02cd2a9f60cbe2cfd25a5ba878d3f8be07417b4d
Nicholas Suski <n33kos@gmail.com>
2016-06-27 00:00:41 -0600

    Added new character.

--- underwatch.p8.lua
+++ underwatch.p8.lua
@@ -38,10 +38,10 @@ currmap.dim.x = 64
 currmap.dim.y = 16
 
 
---------------Characters--------------
+--------------characters--------------
 -------------------------------------
 
-----------------Soldier24-----------------
+----------------soldier24-----------------
 soldier24 = {}
 soldier24.pos = {}
 	soldier24.pos.x = 0
@@ -65,8 +65,8 @@ soldier24.animations = {}
 	soldier24.animations.idle = {0}
 	soldier24.animations.walk = {1,1,1,1,2,2,2,2}
 	soldier24.animations.jump = {1}
-soldier24.movement_behavior = "random"
-soldier24.attack_behavior = "random"
+soldier24.movement_behavior = "follow"
+soldier24.attack_behavior = "primary"
 soldier24.spriteflip = {}
 	soldier24.spriteflip.x = true
 	soldier24.spriteflip.y = false
@@ -106,8 +106,7 @@ soldier24.alternate = {}
 	soldier24.alternate.pixeloffset = {}
 		soldier24.alternate.pixeloffset.x = 0
 		soldier24.alternate.pixeloffset.y = 4
-
---------------------------FilthMouse----------------------
+--------------------------filthmouse----------------------
 filthmouse = {}
 filthmouse.pos = {}
 filthmouse.pos.x = 0
@@ -131,8 +130,8 @@ filthmouse.animations = {}
 	filthmouse.animations.idle = {32}
 	filthmouse.animations.walk = {33,33,33,34,34,34}
 	filthmouse.animations.jump = {33}
-filthmouse.movement_behavior = "random"
-filthmouse.attack_behavior = "random"
+filthmouse.movement_behavior = "follow"
+filthmouse.attack_behavior = "primary"
 filthmouse.spriteflip = {}
 filthmouse.spriteflip.x = true
 filthmouse.spriteflip.y = false
@@ -172,74 +171,140 @@ filthmouse.alternate = {}
 	filthmouse.alternate.pixeloffset = {}
 		filthmouse.alternate.pixeloffset.x = 0
 		filthmouse.alternate.pixeloffset.y = 3
---------------------------Tankdude----------------------
-tankdude = {}
-tankdude.pos = {}
-tankdude.pos.x = 0
-tankdude.pos.y = 0
-tankdude.sca = {}
-tankdude.sca.x = 8
-tankdude.sca.y = 8
-tankdude.velocity = {}
-tankdude.velocity.x = rnd(2)
-tankdude.velocity.y = rnd(2)
-tankdude.mass = 2
-tankdude.speed = 0.1
-tankdude.jumpheight = 5
-tankdude.isjumping = true
-tankdude.ismortal = true
-tankdude.hp = 20
-tankdude.maxhp = 5
-tankdude.sprite = 32
-tankdude.current_animation = "idle"
-tankdude.animations = {}
-	tankdude.animations.idle = {16}
-	tankdude.animations.walk = {17,17,17,18,18,18}
-	tankdude.animations.jump = {17}
-tankdude.movement_behavior = "random"
-tankdude.attack_behavior = "random"
-tankdude.spriteflip = {}
-tankdude.spriteflip.x = true
-tankdude.spriteflip.y = false
-tankdude.shottimer = 0
-tankdude.projectile = {}
-	tankdude.projectile.parent = ""
-	tankdude.projectile.sprite = 19
-	tankdude.projectile.mass = 0.25
-	tankdude.projectile.maxage = 2
-	tankdude.projectile.bounce = false
-	tankdude.projectile.damage = 2
-	tankdude.projectile.firedelay = 4 --draw frames between shots
-	tankdude.projectile.velocity = {}
-		tankdude.projectile.velocity.x = 1
-		tankdude.projectile.velocity.y = 0
-	tankdude.projectile.sca = {}
-		tankdude.projectile.sca.x = 6
-		tankdude.projectile.sca.y = 6
-	tankdude.projectile.pixeloffset = {}
-		tankdude.projectile.pixeloffset.x = 0
-		tankdude.projectile.pixeloffset.y = 0
-tankdude.alternateshottimer = 0
-tankdude.alternate = {}
-	tankdude.alternate.parent = ""
-	tankdude.alternate.sprite = 19
-	tankdude.alternate.mass = 0.25
-	tankdude.alternate.maxage = 2
-	tankdude.alternate.bounce = false
-	tankdude.alternate.damage = 2
-	tankdude.alternate.firedelay = 4 --draw frames between shots
-	tankdude.alternate.velocity = {}
-		tankdude.alternate.velocity.x = 1
-		tankdude.alternate.velocity.y = 0
-	tankdude.alternate.sca = {}
-		tankdude.alternate.sca.x = 6
-		tankdude.alternate.sca.y = 6
-	tankdude.alternate.pixeloffset = {}
-		tankdude.alternate.pixeloffset.x = 0
-		tankdude.alternate.pixeloffset.y = 0
+--------------------------rainheart----------------------
+rainheart = {}
+rainheart.pos = {}
+rainheart.pos.x = 0
+rainheart.pos.y = 0
+rainheart.sca = {}
+rainheart.sca.x = 8
+rainheart.sca.y = 8
+rainheart.velocity = {}
+rainheart.velocity.x = rnd(2)
+rainheart.velocity.y = rnd(2)
+rainheart.mass = 2
+rainheart.speed = 0.1
+rainheart.jumpheight = 5
+rainheart.isjumping = true
+rainheart.ismortal = true
+rainheart.hp = 20
+rainheart.maxhp = 5
+rainheart.sprite = 32
+rainheart.current_animation = "idle"
+rainheart.animations = {}
+	rainheart.animations.idle = {16}
+	rainheart.animations.walk = {17,17,17,18,18,18}
+	rainheart.animations.jump = {17}
+rainheart.movement_behavior = "follow"
+rainheart.attack_behavior = "alternate"
+rainheart.spriteflip = {}
+rainheart.spriteflip.x = true
+rainheart.spriteflip.y = false
+rainheart.shottimer = 0
+rainheart.projectile = {}
+	rainheart.projectile.parent = ""
+	rainheart.projectile.sprite = 19
+	rainheart.projectile.mass = 0.25
+	rainheart.projectile.maxage = 2
+	rainheart.projectile.bounce = false
+	rainheart.projectile.damage = 2
+	rainheart.projectile.firedelay = 4 --draw frames between shots
+	rainheart.projectile.velocity = {}
+		rainheart.projectile.velocity.x = 1
+		rainheart.projectile.velocity.y = 0
+	rainheart.projectile.sca = {}
+		rainheart.projectile.sca.x = 6
+		rainheart.projectile.sca.y = 6
+	rainheart.projectile.pixeloffset = {}
+		rainheart.projectile.pixeloffset.x = 0
+		rainheart.projectile.pixeloffset.y = 0
+rainheart.alternateshottimer = 0
+rainheart.alternate = {}
+	rainheart.alternate.parent = ""
+	rainheart.alternate.sprite = 20
+	rainheart.alternate.mass = 1
+	rainheart.alternate.maxage = 1
+	rainheart.alternate.bounce = false
+	rainheart.alternate.damage = 0
+	rainheart.alternate.firedelay = 1 --draw frames between shots
+	rainheart.alternate.velocity = {}
+		rainheart.alternate.velocity.x = 0
+		rainheart.alternate.velocity.y = 0
+	rainheart.alternate.sca = {}
+		rainheart.alternate.sca.x = 1
+		rainheart.alternate.sca.y = 8
+	rainheart.alternate.pixeloffset = {}
+		rainheart.alternate.pixeloffset.x = 1
+		rainheart.alternate.pixeloffset.y = 0
+--------------------------spiderlady----------------------
+spiderlady = {}
+spiderlady.pos = {}
+spiderlady.pos.x = 0
+spiderlady.pos.y = 0
+spiderlady.sca = {}
+spiderlady.sca.x = 8
+spiderlady.sca.y = 8
+spiderlady.velocity = {}
+spiderlady.velocity.x = rnd(2)
+spiderlady.velocity.y = rnd(2)
+spiderlady.mass = 1
+spiderlady.speed = 0.2
+spiderlady.jumpheight = 7.5
+spiderlady.isjumping = true
+spiderlady.ismortal = true
+spiderlady.hp = 3
+spiderlady.maxhp = 3
+spiderlady.sprite = 5
+spiderlady.current_animation = "idle"
+spiderlady.animations = {}
+	spiderlady.animations.idle = {5}
+	spiderlady.animations.walk = {6,6,6,7,7,7}
+	spiderlady.animations.jump = {6}
+spiderlady.movement_behavior = "follow"
+spiderlady.attack_behavior = "primary"
+spiderlady.spriteflip = {}
+spiderlady.spriteflip.x = true
+spiderlady.spriteflip.y = false
+spiderlady.shottimer = 0
+spiderlady.projectile = {}
+	spiderlady.projectile.parent = ""
+	spiderlady.projectile.sprite = 8
+	spiderlady.projectile.mass = 0.1
+	spiderlady.projectile.maxage = 50
+	spiderlady.projectile.bounce = false
+	spiderlady.projectile.damage = 10
+	spiderlady.projectile.firedelay = 20 --draw frames between shots
+	spiderlady.projectile.velocity = {}
+		spiderlady.projectile.velocity.x = 10
+		spiderlady.projectile.velocity.y = 0
+	spiderlady.projectile.sca = {}
+		spiderlady.projectile.sca.x = 8
+		spiderlady.projectile.sca.y = 1
+	spiderlady.projectile.pixeloffset = {}
+		spiderlady.projectile.pixeloffset.x = 0
+		spiderlady.projectile.pixeloffset.y = 2
+spiderlady.alternateshottimer = 0
+spiderlady.alternate = {}
+	spiderlady.alternate.parent = ""
+	spiderlady.alternate.sprite = 9
+	spiderlady.alternate.mass = 0.1
+	spiderlady.alternate.maxage = 10
+	spiderlady.alternate.bounce = true
+	spiderlady.alternate.damage = 2
+	spiderlady.alternate.firedelay = 10 --draw frames between shots
+	spiderlady.alternate.velocity = {}
+		spiderlady.alternate.velocity.x = 5
+		spiderlady.alternate.velocity.y = -2.5
+	spiderlady.alternate.sca = {}
+		spiderlady.alternate.sca.x = 4
+		spiderlady.alternate.sca.y = 2
+	spiderlady.alternate.pixeloffset = {}
+		spiderlady.alternate.pixeloffset.x = 0
+		spiderlady.alternate.pixeloffset.y = 0
 
 
--------------------entities
+
+--copy tables, yum.
 function copy(o)
 	local c
 	if type(o) == 'table' then
@@ -253,8 +318,9 @@ function copy(o)
 	return c
 end
 
+-------------------entities
 function make_player()
-	temp_entity = copy(tankdude)
+	temp_entity = copy(rainheart)
 	temp_entity.projectile.parent = "team1"
 	temp_entity.alternate.parent = "team1"
 	temp_entity.pos.x = rnd(256)
@@ -263,22 +329,49 @@ function make_player()
 end
 
 function make_ai()
-	for i=1,5 do
-		temp_entity = copy(filthmouse)
-		temp_entity.projectile.parent = "team1"
-		temp_entity.alternate.parent = "team1"
-		temp_entity.pos.x = rnd(256)
-		temp_entity.pos.y = rnd(128) - phys.ground_height
-		add(ai_entities, temp_entity)
-	end
-	for i=1,6 do
-		temp_entity = copy(soldier24)
-		temp_entity.projectile.parent = "team2"
-		temp_entity.alternate.parent = "team2"
-		temp_entity.pos.x = rnd(256)+256
-		temp_entity.pos.y = rnd(128) - phys.ground_height
-		add(ai_entities, temp_entity)
-	end
+	temp_entity = copy(filthmouse)
+	temp_entity.projectile.parent = "team1"
+	temp_entity.alternate.parent = "team1"
+	temp_entity.pos.x = rnd(256)
+	temp_entity.pos.y = rnd(128) - phys.ground_height
+	add(ai_entities, temp_entity)
+	temp_entity = copy(spiderlady)
+	temp_entity.projectile.parent = "team1"
+	temp_entity.alternate.parent = "team1"
+	temp_entity.pos.x = rnd(256)
+	temp_entity.pos.y = rnd(128) - phys.ground_height
+	add(ai_entities, temp_entity)
+	temp_entity = copy(soldier24)
+	temp_entity.projectile.parent = "team1"
+	temp_entity.alternate.parent = "team1"
+	temp_entity.pos.x = rnd(256)
+	temp_entity.pos.y = rnd(128) - phys.ground_height
+	add(ai_entities, temp_entity)
+
+	temp_entity = copy(soldier24)
+	temp_entity.projectile.parent = "team2"
+	temp_entity.alternate.parent = "team2"
+	temp_entity.pos.x = rnd(256)+256
+	temp_entity.pos.y = rnd(128) - phys.ground_height
+	add(ai_entities, temp_entity)
+	temp_entity = copy(rainheart)
+	temp_entity.projectile.parent = "team2"
+	temp_entity.alternate.parent = "team2"
+	temp_entity.pos.x = rnd(256)+256
+	temp_entity.pos.y = rnd(128) - phys.ground_height
+	add(ai_entities, temp_entity)
+	temp_entity = copy(spiderlady)
+	temp_entity.projectile.parent = "team2"
+	temp_entity.alternate.parent = "team2"
+	temp_entity.pos.x = rnd(256)+256
+	temp_entity.pos.y = rnd(128) - phys.ground_height
+	add(ai_entities, temp_entity)
+	temp_entity = copy(filthmouse)
+	temp_entity.projectile.parent = "team2"
+	temp_entity.alternate.parent = "team2"
+	temp_entity.pos.x = rnd(256)+256
+	temp_entity.pos.y = rnd(128) - phys.ground_height
+	add(ai_entities, temp_entity)
 end
 
 function make_projectile(sprite, mass, bounce, damage, sca, pos, velocity, maxage, direction, parent)
@@ -349,7 +442,7 @@ function ai_movement_behavior(entity)
 end
 
 function ai_attack_behavior(entity)
-	if entity.attack_behavior == "random" then
+	if entity.attack_behavior == "primary" then
 		--counter
 		entity.shottimer -= 1
 
@@ -365,6 +458,28 @@ function ai_attack_behavior(entity)
 		if entity.shottimer <= 0 then
 			entity.shottimer = entity.projectile.firedelay
 			make_projectile(entity.projectile.sprite, entity.projectile.mass, entity.projectile.bounce, entity.projectile.damage, entity.projectile.sca, adjustedpos, entity.projectile.velocity, entity.projectile.maxage, entity.spriteflip.x, entity.projectile.parent)
+		end
+	elseif entity.attack_behavior == "alternate" then
+		--counter
+		entity.alternateshottimer -= 1
+
+		--adjust bullet start location
+		adjustedpos = {}
+		if entity.spriteflip.x then
+			adjustedpos.x = entity.pos.x-entity.sca.x-entity.alternate.pixeloffset.x+1
+		else
+			adjustedpos.x = entity.pos.x+entity.sca.x+entity.alternate.pixeloffset.x
+		end
+		adjustedpos.y = entity.pos.y+entity.alternate.pixeloffset.y
+
+		if entity.alternateshottimer <= 0 then
+			entity.alternateshottimer = entity.alternate.firedelay
+			make_projectile(entity.alternate.sprite, entity.alternate.mass, entity.alternate.bounce, entity.alternate.damage, entity.alternate.sca, adjustedpos, entity.alternate.velocity, entity.alternate.maxage, entity.spriteflip.x, entity.alternate.parent)
+		end
+
+		--probably a better way to do this
+		if entity.alternate.damage == 0 then
+			entity.ismortal = false
 		end
 	end
 end
@@ -452,8 +567,10 @@ function projectile_collision(entity, parenttype)
 		if(a>w and c>w and a>y and c>y) intersect = false
 		if(b<x and d<x and b<z and d<z) intersect = false
 		if(b>x and d>x and b>z and d>z) intersect = false
-		if intersect and bullet.parent != entity.projectile.parent and entity.ismortal then
-			entity.hp -= bullet.damage
+		if intersect and bullet.parent != entity.projectile.parent then
+			if entity.ismortal then
+				entity.hp -= bullet.damage
+			end
 			del(projectiles, bullet)
 		end
 	end
@@ -477,8 +594,6 @@ function move_camera()
 end
 
 function set_animation_frame(entity)
-	
-	
 	if entity.isjumping and entity.velocity.y < 0 then
 		entity.current_animation = "jump"
 	elseif entity.velocity.x > 0.2 or entity.velocity.x < -0.2 then
@@ -621,6 +736,18 @@ function _draw()
 				entity.alternateshottimer = entity.alternate.firedelay
 				make_projectile(entity.alternate.sprite, entity.alternate.mass, entity.alternate.bounce, entity.projectile.damage, entity.alternate.sca, adjustedpos, entity.alternate.velocity, entity.alternate.maxage, entity.spriteflip.x, entity.alternate.parent)
 			end
+
+			--probably a better way
+			if entity.alternate.damage == 0 then
+				entity.ismortal = false
+			end
+		end
+	else
+		for key,entity in pairs(player_entities) do
+			--probably a better way
+			if entity.alternate.damage == 0 then
+				entity.ismortal = true
+			end
 		end
 	end
 
@@ -649,30 +776,30 @@ end
 
 
 __gfx__
-066f0000066f0000066f000095500000ac6600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-06f8000006f8000006f8000000000000915500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-01150000011500000115000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-0cc666660cc666660cc6666600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-0c1156000c1156000c11560000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-05550000055500000555000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-05050000050500000505000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-06060000600600000660000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-0000600c0000600c0000600c06600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-0066600c0066600c0066600c00665000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-6065a06c6065a06c6065a06c00656500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-6665566c6665566c6665566c06505000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-5666665c5666665c5666665c65000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-05f560fc05f560fc05f560fc50000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-0056500c0056500c0056500c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-0050500c0500500c0055000c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+066f0000066f0000066f000095500000ac660000225500002255000022550000777777660cc00000000000000000000000000000000000000000000000000000
+06f8000006f8000006f8000000000000915500002258000022580000225800000000000050050000000000000000000000000000000000000000000000000000
+01150000011500000115000000000000000000002e6666662e6666662e6666660000000000000000000000000000000000000000000000000000000000000000
+0cc666660cc666660cc6666600000000000000002e5650002e5650002e5650000000000000000000000000000000000000000000000000000000000000000000
+0c1156000c1156000c1156000000000000000000055e0000055e0000055e00000000000000000000000000000000000000000000000000000000000000000000
+055500000555000005550000000000000000000006e6000006e6000006e600000000000000000000000000000000000000000000000000000000000000000000
+05050000050500000505000000000000000000000505000005050000050500000000000000000000000000000000000000000000000000000000000000000000
+06060000600600000660000000000000000000000505000050050000055000000000000000000000000000000000000000000000000000000000000000000000
+00000600000006000000060006600000c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+00066600000666000006660000665000c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+06065a0606065a0606065a0600656500c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+06665566066655660666556606505000c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+05666665056666650566666565000000c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+005f560f005f560f005f560f50000000c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+00056500000565000005650000000000c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+00050500005005000005500000000000c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 0009a9000009a9000009a90009000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 000af400000af400000af40098900000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 060fff00060fff00060fff0009000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 005a555a005a555a005a555a00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 005fff50005fff50005fff5000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 06099900060999000609990000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-00090700000907000009070000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-00070700007007000007700000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+00090700000907000009070000000000006886000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+00070700007007000007700000000000055555500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 3b3b3b334444444411111111dddddddd888888880000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 b333b33b4444444411111111ddddddddeeeeeeee0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 353353354444444411111111dddddddde22ee2e20000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
